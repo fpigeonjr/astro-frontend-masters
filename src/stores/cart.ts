13fe4911a344eecc98f48da4008988f0e221a0c8
fpigeonjr <frank.pigeonjr@gmail.com>
2023-10-15 23:21:57 -0500

    adds microstore

---
+++ src/stores/cart.ts
@@ -0,0 +1,32 @@
+import { computed, map } from 'nanostores';
+
+export const $cart = map<Record<number, CartItem>>({});
+
+export const addItemToCart = (item: ShopItem) => {
+  const cartItem = $cart.get()[item.id];); 
+  const quantity = cartItem ? cartItem.quantity  : 0;
+
+  $cart.setKey(item.id, {
+    item,
+    quantity: quantity + 1,
+
+  })
+}
+
+export const removeItemFromCart = (itemId: number) => {
+  // @ts-ignore
+  $cart.setKey(itemId, undefined);
+}
+
+export const subtotal = computed(cart, (entries) => {
+	let subtotal = 0;
+	Object.values(entries).forEach((entry) => {
+		if (!entry) {
+			return subtotal;
+		}
+
+		subtotal += entry.quantity * entry.item.price;
+	});
+
+	return subtotal;
+});
