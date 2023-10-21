37d0e4557e859583439821da6177f9856450ebaa
fpigeonjr <frank.pigeonjr@gmail.com>
2023-10-21 16:26:13 -0500

    adds cart logix

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,22 +3,21 @@ import { computed, map } from 'nanostores';
 export const $cart = map<Record<number, CartItem>>({});
 
 export const addItemToCart = (item: ShopItem) => {
-  const cartItem = $cart.get()[item.id];); 
-  const quantity = cartItem ? cartItem.quantity  : 0;
+	const cartItem = $cart.get()[item.id];
+	const quantity = cartItem ? cartItem.quantity : 0;
 
-  $cart.setKey(item.id, {
-    item,
-    quantity: quantity + 1,
-
-  })
-}
+	$cart.setKey(item.id, {
+		item,
+		quantity: quantity + 1,
+	});
+};
 
 export const removeItemFromCart = (itemId: number) => {
-  // @ts-ignore
-  $cart.setKey(itemId, undefined);
-}
+	// @ts-ignore
+	$cart.setKey(itemId, undefined);
+};
 
-export const subtotal = computed(cart, (entries) => {
+export const subtotal = computed($cart, (entries) => {
 	let subtotal = 0;
 	Object.values(entries).forEach((entry) => {
 		if (!entry) {
